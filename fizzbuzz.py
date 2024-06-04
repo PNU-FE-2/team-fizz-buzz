3852659c4c784a60c42ef37ea9702d4be11a22a3
Jisu Lee <dlwltn0430@naver.com>
2024-06-04 20:43:54 +0900

    fix: 충돌 해결

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,5 @@
 for i in range(1, 101):
-    if i % 5 == 0:
-        print("Buzz")
-    elif i % 15 == 0:
+    if i % 15 == 0:
         print('FizzBuzz')
     elif i % 5 == 0:
         print("Buzz")
